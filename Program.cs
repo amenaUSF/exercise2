da5b1d6a7e99802b3b6f8d5196d4921553d547d6
Amena Shahid <amenashahid@usf.edu>
2020-01-21 20:57:55 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            int height;
+            //max heart rate is equal to 220 - age
+
+            age = 29;
+            height = 60;
+
+            Console.WriteLine("I am " + age + " years old, my max heart rate is " + (220 - age) + " and I am " + height + " inches tall");
+            
+        }
+    }
+}
